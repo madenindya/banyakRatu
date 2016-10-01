b26d59273b7d91cb11f1b54d148f63e0435c8292
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-10-01 08:24:36 +0700

    Add RowAssignment

--- Cnf.java
+++ Cnf.java
@@ -53,3 +53,24 @@ public class Cnf {
 		return (N * (row - 1)) + column;
 	}
 }
+
+public static void RowAssignment(int n){
+  /*
+  Column Assignments
+  */
+  String sub1 = "";
+  String sub2 = "";
+  for(int j = 1; j <= n; j++){
+    for(int i = 1; i <= n; i++){
+      if(i == n){
+        sub1 += (j-1)*n+i + " 0\n";
+        sub2 += -1*((j-1)*n+i) + " 0\n";
+      }else{
+        sub1 += (j-1)*n+i + " ";
+        sub2 += -1*((j-1)*n+i) + " ";
+       }
+      }
+    }
+  String result = sub1 + sub2;
+  System.out.println(result);
+}
