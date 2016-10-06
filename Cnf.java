a77a5b0d6f0045c6add07d88f5e8a89153a046f7
madenindya <made.nindyatama@gmail.com>
2016-10-06 11:28:59 +0700

    tmp fix colum

--- Cnf.java
+++ Cnf.java
@@ -43,42 +43,42 @@ public class Cnf {
 				int tmp = i > j2 ? i : j2;
 				for (int k = 1; k <= n - tmp; k++) {
 					int now2 = getIndex(i + k, j - k);
-					System.out.println("-" + now + " -" + now2 +" 0");
+					System.out.println("-" + now + " -" + now2 + " 0");
 				}
 			}
 		}
 	}
 
-	public static void columnAssignment(int n){
-		  /*
-		  Column Assignments
-		  */
-		  String sub1 = "";
-			String sub2 = "";
-			int hold_var = 2;
-			for(int j = 1; j <= n; j++){
-		    for(int i = 1; i <= n; i++){
-		      if(i == n){
-		        sub1 += getIndex(j, i)+ " 0\n";
-		      }else{
-		        sub1 += getIndex(j, i) + " ";
-		       }
-		      }
-		    }
-				for(int i = 1; i <= n; i++){
-					for(int j = 1; j < n; j++){
-						sub2 += -getIndex(i,1) + " " + -getIndex(i,j+1) + " 0\n";
-						if(hold_var < n){
-							sub2 += -getIndex(i,hold_var) + " " + -getIndex(i,hold_var + 1) + " 0\n";
-						}
-						hold_var++;
-						}
-						hold_var = 2;
-					}
-
-		  String result = sub1 + sub2;
-		  System.out.println(result);
+	public static void columnAssignment(int n) {
+		/*
+		 * Column Assignments
+		 */
+		String sub1 = "";
+		String sub2 = "";
+		
+		// Assignment value
+		for (int j = 1; j <= n; j++) {
+			for (int i = 1; i <= n; i++) {
+				if (i == n) {
+					sub1 += getIndex(j, i) + " 0\n";
+				} else {
+					sub1 += getIndex(j, i) + " ";
+				}
+			}
 		}
+		
+		// column restriction
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j < n; j++) {
+				for (int k = j+1; k <= n; k++) {
+					sub2 += -getIndex(i, j) + " " + -getIndex(i, k) + " 0\n";
+				}
+			}
+		}
+
+		String result = sub1 + sub2;
+		System.out.print(result);
+	}
 
 	public static int getIndex(int row, int column) {
 		return (N * (row - 1)) + column;
